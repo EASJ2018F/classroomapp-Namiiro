290c0c0010fe6b7228b2fda452c992b8aa3fedd5
Roseiam <Queennamiiro@hotmail.com>
2018-01-29 13:44:15 +0100

    Jeg har fulgt punkterne i opgave 1 og løst opgaven.

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    {
+        private string _klasseNavn;
+        private List<Students> _klasseListe;
+        private DateTime _semesterStart; 
+
+        
+    }
+}
---
+++ ClassRoom/Students.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Students
+    {
+        private string _name;
+        private int _month;
+        private int _day;
+
+        public Students(string firstName, int calenderMonth, int calenderDay)
+        {
+            _name = firstName;
+            _month = calenderMonth;
+            _day = calenderDay; 
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        
+        public int CalenderMonth
+        {
+            get;
+            set;
+        }
+
+        public int CalenderDay
+        {
+            get;
+            set;
+        }
+    }
+}
