3896b4c16d80869fd470c8041dfc57bb1d0c94f1
Roseiam <Queennamiiro@hotmail.com>
2018-01-29 15:25:27 +0100

    Jeg har nu løst opgave 6

--- ClassRoom/KlasseRum.cs
+++ ClassRoom/KlasseRum.cs
@@ -35,5 +35,40 @@ namespace ClassRoom
             get { return _semesterStart; }
            
         }
+
+        public void BirthSeason()
+        {
+            int winter = 0;
+            int spring = 0;
+            int summer = 0;
+            int autumn = 0;
+
+            foreach (var student in KlasseListe)
+            {
+                if(student.BirthSeason() == "winter")
+                {
+                    winter += 1;
+                }
+                if(student.BirthSeason() == "spring")
+                {
+                    spring += 1;
+                }
+                if(student.BirthSeason() == "summer")
+                {
+                    summer += 1;
+                }
+                if (student.BirthSeason() == "autumn")
+                {
+                    autumn += 1;
+                }
+            }
+
+            Console.WriteLine($"Born in winter: {winter}");
+            Console.WriteLine($"Born in spring: {winter}");
+            Console.WriteLine($"Born in summer: {winter}");
+            Console.WriteLine($"Born in autumn: {winter}");
+
+
+        }
     }
 }
